0a6cfe91e5187235dbec719c165b863d7758d294
guosw <guoshaowei@pvmedtech.com>
2020-03-16 15:01:30 +0800

    updated at 2020-03-16 15:01:30

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,12 @@ module.exports = {
   port: '4040', // 开发端口
   title: 'guodada-note',
   description: '郭大大的个人笔记',
+  head: [
+    // add jquert and fancybox
+    ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js' }],
+    ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.js' }],
+    ['link', { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.css' }]
+  ],
   themeConfig: {
     smoothScroll: true,
     sidebarDepth: 2,
---
+++ docs/.vuepress/theme/components/Page.vue
@@ -0,0 +1,55 @@
+<template>
+  <main class="page">
+    <slot name="top" />
+
+    <Content class="theme-default-content" />
+    <PageEdit />
+
+    <PageNav v-bind="{ sidebarItems }" />
+
+    <slot name="bottom" />
+  </main>
+</template>
+
+<script>
+import PageEdit from '@theme/components/PageEdit.vue'
+import PageNav from '@theme/components/PageNav.vue'
+
+export default {
+  components: { PageEdit, PageNav },
+  props: ['sidebarItems'],
+  methods: {
+    createEditLink(repo, docsRepo, docsDir, docsBranch, path) {},
+
+    // 图片放大方法，用代码给img标签添加a标签
+    setPictureZoom() {
+      let img = document.querySelectorAll('p > img')
+      for (let i = 0; i < img.length; i++) {
+        if (img[i]) {
+          let a = document.createElement('a')
+          let parent = img[i].parentNode
+          parent.appendChild(a)
+          let src = img[i].getAttribute('src')
+          a.setAttribute('data-fancybox', '')
+          a.setAttribute('href', src)
+          a.appendChild(img[i])
+        }
+      }
+    }
+  },
+  mounted() {
+    this.setPictureZoom()
+  },
+  updated() {
+    this.setPictureZoom()
+  }
+}
+</script>
+
+<style lang="stylus">
+@require '../styles/wrapper.styl'
+
+.page
+  padding-bottom 2rem
+  display block
+</style>
