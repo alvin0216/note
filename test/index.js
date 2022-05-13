4c328fe26693dfd4b9aa51f233362ccee16b1f3c
alvin <alvin00216@163.com>
2022-05-13 22:15:37 +0800

    updated at 2022-05-13 22:15:37

--- test/index.js
+++ test/index.js
@@ -8,31 +8,17 @@
  */
 /**
  * @param {TreeNode} root
- * @param {number} key
  * @return {TreeNode}
  */
-var deleteNode = function (root, key) {
-  if (!root) return null;
-  if (key > root.val) root.right = deleteNode(root.right, key);
-  else if (key < root.val) root.left = deleteNode(root.left, key);
-  else {
-    // 删除的是当前节点
-    if (!root.left) return root.right;
-    else if (!root.right) return root.left;
-    else {
-      // 左右子树都存在的情况下
-      let newRoot = root.right;
-
-      // 找到右子树下的最左子树，将原本的 root.left 指向这个节点
-      let cur = root.right;
-      while (cur.left) {
-        cur = cur.left;
-      }
-      cur.left = root.left;
-
-      return newRoot;
-    }
+var convertBST = function (root) {
+  let num = 0;
+  function dfs(node) {
+    if (!node) return;
+    dfs(node.right);
+    node.val = node.val + num;
+    num = node.val;
+    dfs(node.left);
+    return node;
   }
-
-  return root;
+  return dfs(root);
 };
