bdef6340e101408fab2f6dbd3a97fab17d56c0c0
alvin <alvin00216@163.com>
2022-06-03 00:14:20 +0800

    updated at 2022-06-03 00:14:20

---
+++ .vuepress/sidebar.js
@@ -0,0 +1,273 @@
+module.exports = {
+  '/浏览器/': [
+    '浏览器如何解析html',
+    'WebComponent',
+    {
+      title: 'Browser',
+      children: [
+        'token-cookie-session',
+        'cookie',
+        'token',
+        'cache',
+        'safe',
+        {
+          title: '🔐 浏览器安全',
+          children: ['cross-origin', 'xss', 'csrf'],
+        },
+      ],
+    },
+  ],
+
+  '/算法/': [
+    {
+      title: '字符串',
+      children: [
+        '字符串/千位分隔数',
+        '字符串/反转字符串II',
+        '字符串/最长不含重复字符的子字符串',
+        '字符串/字符串的排列',
+        '字符串/最小覆盖子串',
+        '字符串/回文系列',
+      ],
+    },
+    {
+      title: '数组',
+      children: ['数组/两数之和', '数组/合并两个有序数组', '数组/长度最小的子数组', '数组/双指针'],
+    },
+    {
+      title: '链表',
+      children: [
+        '链表/移除链表元素',
+        '链表/删除链表的倒数第n个结点',
+        '链表/反转链表',
+        '链表/环形链表II',
+        '链表/相交链表',
+        '链表/k个一组翻转链表',
+      ],
+    },
+    {
+      title: '二叉树',
+      children: [
+        '二叉树/二叉树的遍历方式',
+        '二叉树/二叉树的属性',
+        '二叉树/二叉树的修改与构造',
+        '二叉树/二叉树的修改与构造2',
+        '二叉树/求二叉搜索树的属性',
+        '二叉树/二叉树的公共祖先',
+        '二叉树/二叉树其他题目',
+      ],
+    },
+    {
+      title: '动态规划',
+      children: [
+        '动态规划/动态规划理论基础',
+        '动态规划/爬楼梯',
+        '动态规划/不同路径',
+        '动态规划/打家劫舍系列',
+        '动态规划/买卖股票的最佳时机',
+        '动态规划/连续',
+      ],
+    },
+    {
+      title: '回溯算法',
+      children: [
+        '回溯/总结',
+        '回溯/回溯排列',
+        '回溯/回溯分割',
+        '回溯/子集',
+        '回溯/子集II',
+        '回溯/组合总和',
+        '回溯/N皇后',
+      ],
+    },
+    {
+      title: '贪心算法',
+      children: ['贪心算法/贪心入门'],
+    },
+    {
+      title: '深度遍历',
+      children: ['深度遍历/岛屿数量', '深度遍历/单词搜索', '深度遍历/螺旋矩阵'],
+    },
+    {
+      title: '算法基础',
+      children: [
+        'binarySearch',
+        'bfs-dfs',
+        'math',
+        {
+          title: '💭 数据结构',
+          children: [
+            'data-structure/stack',
+            'data-structure/queue',
+            'data-structure/linkedList',
+            'data-structure/set',
+            'data-structure/dict-hashtable',
+            'data-structure/tree',
+            'data-structure/avl',
+            'data-structure/graph',
+            'data-structure/graph-traverse',
+          ],
+        },
+        {
+          title: '👉 排序算法',
+          children: [
+            'sort/bubbleSort',
+            'sort/selectionSort',
+            'sort/insertSort',
+            'sort/mergeSort',
+            'sort/quickSort',
+            'sort/shellSort',
+            'sort/heapSort',
+          ],
+        },
+        {
+          title: '🧠 一些算法思想',
+          children: ['method/complexity', 'method/sliding-window', 'method/divide', 'method/shuffle'],
+        },
+      ],
+    },
+  ],
+
+  '/react/': [
+    {
+      title: 'React',
+      children: ['', 'immer', 'hooks-api', 'mini', 'setState', 'concurrent', 'diff', 'synthetic-event'],
+    },
+  ],
+
+  '/网络协议/': [
+    {
+      title: '计算机基础',
+      children: [
+        '',
+        'network-model',
+        'dns',
+        'cdn',
+        {
+          title: '💭 TCP',
+          children: ['tcp/structure', 'tcp/tcp-udp', 'tcp/handshake', 'tcp/wave', 'tcp/sync-attack'],
+        },
+        {
+          title: '🏷️ HTTP',
+          children: [
+            'http/structure',
+            'http/request-method',
+            'http/status-code',
+            'http/request-header',
+            'http/desc',
+            'http/cookie',
+            'http/blocking',
+          ],
+        },
+        {
+          title: '🤓️ HTTPS',
+          children: ['https/https', 'https/tsl', 'https/ca'],
+        },
+        {
+          title: '🐈 HTTP2',
+          children: ['http2/http2-vs-http', 'http2/frame', 'http2/http3', 'http2/push'],
+        },
+      ],
+    },
+  ],
+
+  '/工程化/': [
+    {
+      title: 'Devops',
+      children: [
+        '',
+        'grayscale-release',
+        'high-concurrency',
+        '沙箱的实现方式',
+        {
+          title: '🍞 包管理',
+          children: ['packages/pnpm', 'packages/npm', 'packages/lerna'],
+        },
+        {
+          title: '🔧 持续集成',
+          children: ['ci/travis', 'ci/webhooks', 'ci/github-action'],
+        },
+        {
+          title: '🐒 环境配置',
+          children: ['config/mac', 'config/ssh', 'config/certificate', 'config/charles'],
+        },
+        {
+          title: '🥣 nginx',
+          children: ['nginx/basics', 'nginx/proxy'],
+        },
+      ],
+    },
+    {
+      title: '性能优化',
+      children: ['performance/what-is-perf'],
+    },
+    {
+      title: '打包工具',
+      children: ['build/模块联邦', 'build/esbuild'],
+    },
+    {
+      title: '监控系列',
+      children: ['monitor/'],
+    },
+  ],
+
+  '/javascript/': [
+    {
+      title: 'Javascript',
+      children: [
+        'awsome',
+        'this',
+        'closure',
+        'prototype',
+        'extends',
+        'for-of',
+        'setTimeout',
+        'requestAnimationFrame',
+        'data-types',
+        'eval',
+        'proxy',
+      ],
+    },
+    {
+      title: '代码实现系列',
+      children: ['achieve/call-apply', 'achieve/bind', 'achieve/promise'],
+    },
+    {
+      title: 'v8 - 引擎工作原理',
+      children: ['v8/run-js', 'v8/context-stack', 'v8/scope', 'v8/scope-chain', 'v8/gc', 'v8/compile', 'v8/eventloop'],
+    },
+  ],
+
+  '/docs/': [
+    '',
+    { title: 'HTML', children: ['html/tag', 'html/script', 'html/iframe'] },
+    {
+      title: 'CSS',
+      children: ['css/center', 'css/bfc', 'css/drop-shadow', 'css/transform'],
+    },
+    {
+      title: '杂七杂八',
+      children: [
+        'awsome/oAuth2',
+        'awsome/oAuth2-github',
+        {
+          title: '🍪 低代码平台',
+          children: ['awsome/lowcode/', 'awsome/lowcode/problem'],
+        },
+        { title: '😍 Typescript', children: ['awsome/typescript/', 'awsome/typescript/react'] },
+        { title: '🙃️ 面试系列', children: ['awsome/interview/', 'awsome/interview/hr'] },
+      ],
+    },
+    {
+      title: '疑难杂症',
+      children: [
+        '疑难杂症/canvas 跨域',
+        '疑难杂症/url 转为 Base64',
+        '疑难杂症/lerna 多包管理发布问题',
+        '疑难杂症/nginx gzip 不生效',
+        '疑难杂症/iframe 打开全屏无效',
+        '疑难杂症/远程组件加载方案',
+      ],
+    },
+  ],
+};
--- docs/index.md
+++ docs/index.md
@@ -1,39 +1,65 @@
 ---
-title: 关于我
-# sidebar: 'auto'
-date: 2021-01-01 18:42:09
+title: 🔥 技术漫谈
+date: 2021-07-22 09:23:33
+sidebar: auto
 ---
 
-:::tip 介绍
+## 实现系列
 
-我是 alvin，热爱编程，乐于分享。
+- [实现 commonjs](https://github.com/alvin0216/note/tree/master/.demo/packages/commonJS)
+- [github 授权登录](https://github.com/alvin0216/note/tree/master/.demo/packages/github-oauth2)
+- [lerna + workspaces](https://github.com/alvin0216/note/tree/master/.demo/packages/lerna-demo)
+- [实现 Promise A+](https://github.com/alvin0216/note/tree/master/.demo/packages/my-promise)
+- [简单实现 react-router](https://github.com/alvin0216/note/tree/master/.demo/packages/my-react-router)
+- [简单实现 react-redux](https://github.com/alvin0216/note/tree/master/.demo/packages/my-redux)
+- [react ssr 搭建 demo](https://github.com/alvin0216/note/tree/master/.demo/packages/react-ssr-demo)
+- [vdom diff](https://github.com/alvin0216/note/tree/master/.demo/packages/vdom-diff)
+- [vite demo](https://github.com/alvin0216/note/tree/master/.demo/packages/vite-ts-demo)
+- [webrtc-demo 视频通话](https://github.com/alvin0216/note/tree/master/.demo/packages/webrtc-demo)
+- [github action demo](https://github.com/alvin0216/note/tree/master/.demo/packages/github-action-demo)
+- [github webhook demo](https://github.com/alvin0216/note/tree/master/.demo/packages/github-webhook-demo)
+- 手撕
+  - 手写快排
+  - 手写继承
+  - 手写深拷贝
+  - 手写 useReducer
+  - 手写节流和防抖
+  - 手写 useDidUpdate
+  - 手写 call apply
+  - 手写 usePreious
+  - 手写 bind
+  - 实现 let 关键字
+  - 手写 Promise
+  - 实现 const 关键字
+  - 实现 sleep
+  - 手写 new
+  - 实现 compose
+  - 手写 instanceof.js
 
-前端开发者，较为熟悉的技术在 `React` 相关...
+<!-- ## 收藏夹
 
-:::
+- [面试真题，会 80% 直接进大厂](https://m26bxrpatp.feishu.cn/base/appcn5mUun8tTLsaFG0jrTeUnBg?table=tbllAUETZhGVTWMA&view=vewJHSwJVd)
+- [我的前端性能优化知识体系](https://juejin.cn/post/7063754993072865287)
+- [得物 AppH5 秒开优化实战](https://mp.weixin.qq.com/s/c66wNbyIJ1GXbLz4Te4qDg)
+- [探索 webpack5 新特性 Module federation 在腾讯文档的应用](http://www.alloyteam.com/2020/04/14338/#prettyPhoto)
+- [Webapck5 核心打包原理全流程解析](https://juejin.cn/post/7031546400034947108) -->
 
-## 开源项目
-
-- [react-blog](https://github.com/alvin0216/react-blog) ：使用 `react hooks + antd + koa2 + mysql` 构建的博客项目
-  - ![Github stars](https://img.shields.io/github/stars/alvin0216/react-blog.svg)
-  - [快速预览](https://blog.alvin.run)
-
-keep doing...
-
-## 未来计划
-
-- 保持学习算法和数据结构
-- 做一个自己的开源项目
-- 精彩的生活
+<!-- 1.  手写快排.js
+1.  手写继承.js
+2.  手写深拷贝.js
+3.  手写 useReducer.js
+4.  手写节流和防抖.js
+5.  手写 useDidUpdate.js
+6.  手写 call apply.js
+7.  手写 usePreious.js
+8.  手写 bind.js
+9.  实现 let 关键字.js
+10. 手写 Promise.js
+11. 实现 const 关键字.js
+12. 手写 Promise.all race.js
+13. 实现 sleep.js
+14. 手写 new.js
+15. 实现 compose.js
+16. 手写 instanceof.js -->
 
 ---
-
-- 开源低代码平台解决方案
-- 开源 ssr blog
-
-## 联系我
-
-有什么好的意见，或者是想要咨询一些事情可以通过以下找到我
-
-- 邮箱：alvin00216@163.com
-- QQ：434358603
