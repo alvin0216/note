068f639567c3061f59a3a9ab064df29781bbfee5
alvin <alvin00216@163.com>
2022-09-09 00:14:54 +0800

    updated at 2022-09-09 00:14:54

--- learning/learn-electron/base1/main.js
+++ learning/learn-electron/base1/main.js
@@ -1,8 +1,7 @@
-// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow() {
+const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -12,32 +11,32 @@ function createWindow() {
     },
   });
 
-  // and load the index.html of the app.
+  // 加载 index.html
   mainWindow.loadFile('index.html');
 
-  // Open the DevTools.
+  // 打开开发工具
   mainWindow.webContents.openDevTools();
-}
+};
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// 这段程序将会在 Electron 结束初始化
+// 和创建浏览器窗口的时候调用
+// 部分 API 在 ready 事件触发后才能使用。
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', function () {
+  app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
 
 // In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// code. 也可以拆分成几个文件，然后用 require 导入。
---
+++ learning/learn-electron/base1/preload.js
@@ -0,0 +1,12 @@
+// All the Node.js APIs are available in the preload process.
+// 它拥有与Chrome扩展一样的沙盒。
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
